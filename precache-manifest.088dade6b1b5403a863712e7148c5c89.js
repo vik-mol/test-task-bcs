d65da48dc853abd0b8301c941cf4da841173b7ed
vik-mol <viktorvmoll@gmail.com>
2020-06-18 16:17:48 +0700

    Updates

---
+++ precache-manifest.088dade6b1b5403a863712e7148c5c89.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e5bf3740245d4dd2ee99d257ec6b68",
+    "url": "/test-task-bcs/index.html"
+  },
+  {
+    "revision": "b0fce20800f75e57c52e",
+    "url": "/test-task-bcs/static/css/2.324a12d4.chunk.css"
+  },
+  {
+    "revision": "e8bba0b8615986e52917",
+    "url": "/test-task-bcs/static/css/main.a442a95d.chunk.css"
+  },
+  {
+    "revision": "b0fce20800f75e57c52e",
+    "url": "/test-task-bcs/static/js/2.5c823e26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test-task-bcs/static/js/2.5c823e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8bba0b8615986e52917",
+    "url": "/test-task-bcs/static/js/main.9b05cc1c.chunk.js"
+  },
+  {
+    "revision": "76eab9bd2475840f0c0a",
+    "url": "/test-task-bcs/static/js/runtime-main.4a3d0839.js"
+  },
+  {
+    "revision": "45656b73e1a07f979bfb56559b359b07",
+    "url": "/test-task-bcs/static/media/Geometria-Medium.45656b73.woff"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/test-task-bcs/static/media/Geometria-Medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "90152a4f5c76144f11d47dcf43ae12d9",
+    "url": "/test-task-bcs/static/media/Geometria-Medium.90152a4f.eot"
+  },
+  {
+    "revision": "3c6a7d3ae76a5aec78aac69db1af15cf",
+    "url": "/test-task-bcs/static/media/Geometria.3c6a7d3a.woff"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/test-task-bcs/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "778525f6405ea0daa98d885a4ba17189",
+    "url": "/test-task-bcs/static/media/Geometria.778525f6.eot"
+  },
+  {
+    "revision": "127c337c0bdccbe16d0ec3892728c6a0",
+    "url": "/test-task-bcs/static/media/logo.127c337c.svg"
+  },
+  {
+    "revision": "f81ce245a526d74af4abf3297fb47c04",
+    "url": "/test-task-bcs/static/media/logoM.f81ce245.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/test-task-bcs/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/test-task-bcs/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/test-task-bcs/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/test-task-bcs/static/media/slick.f97e3bbf.svg"
+  }
+]);
